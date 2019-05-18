b356aeb2b82bfd57261ad353d6ea48cc514d37ed
Agota <agota.kazenaite@yahoo.com>
2019-05-18 18:39:31 +0300

    testing DB

--- src/main/java/com/agota/share_a_dog/entity/Dog.java
+++ src/main/java/com/agota/share_a_dog/entity/Dog.java
@@ -1,7 +1,19 @@
 package com.agota.share_a_dog.entity;
 
 public class Dog {
-    Long id;
-    String name;
-    
+    private Integer id;
+    private String name;
+
+    public  Dog(){
+
+    }
+
+    public Dog(Integer id, String name){
+        this.id = id;
+        this.name = name;
+    }
+
+    public String getName() {
+        return name;
+    }
 }
